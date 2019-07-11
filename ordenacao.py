70b5cd081b7e7a59cc1358d721fb7c107b8ec478
Joubert Filho <37818492+joubertfw@users.noreply.github.com>
2019-07-11 06:23:02 -0300

    Update ordenacao.py

--- ordenacao.py
+++ ordenacao.py
@@ -34,9 +34,28 @@ def mergeSort(arr = []):
         mergeSort(left)
         mergeSort(right)
         
+        i = j = k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i=i+1
+            else:
+                arr[k] = right[j]
+                j=j+1
+            k=k+1
 
+        while i < len(left):
+            arr[k] = left[i]
+            i=i+1
+            k=k+1
 
-    return []
+        while j < len(right):
+            arr[k] = right[j]
+            j=j+1
+            k=k+1
+
+    return arr
 
 
 #OK
