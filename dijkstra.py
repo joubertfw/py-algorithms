97f6e1942b9604e4d2ef72a9f395ccc653a84283
Joubert Filho <37818492+joubertfw@users.noreply.github.com>
2019-08-01 09:08:17 -0300

    Added Vertex, pointing to an Node

--- dijkstra.py
+++ dijkstra.py
@@ -1,16 +1,33 @@
-import math as m
+import math
 
 class node():
-    def __init__(self, nodeId = 0, nodes = [], vextexs = [], visited = False):
+    def __init__(self, nodeId = 0, vertex = [], visited = False, cost = math.inf):
         self.nodeId = nodeId
-        self.nextNode = nextNode
-        self.vextexCost = nextNvextexCostode
+        self.vertex = vertex
         self.visited = visited
+        self.cost = cost
 
-# entrada: "a, b, c" onde a = idOrigem, b = idDestino, c= custo de a para b
+    def __repr__(self):
+      return "({})".format(self.nodeId)
 
-graph = node(nodeId = 1, vextexCost = 0, visited = False)
+class vertex():
+  def __init__(self, nodeTarget = None, distance = 1):
+    self.nodeTarget = nodeTarget
+    self.distance = distance
 
-for i in input:
-    x = node
+  def __repr__(self):
+    return "{}={}".format(self.nodeTarget, self.distance)
 
+# entrada: "a, b, c" onde a = idOrigem, b = idDestino, c = distancia de a para b
+# origin = node(nodeId = a)
+# target = node(nodeId = b)
+# vertex(nodeOrigin = origin, nodeTarget = target, distance = c)
+
+a, b, c, d = node(nodeId = "a"), node(nodeId = "b"), node(nodeId = "c"), node(nodeId = "d")
+
+a.vertex.append(vertex(nodeTarget = b, distance = 5))
+a.vertex.append(vertex(nodeTarget = c, distance = 6))
+b.vertex.append(vertex(nodeTarget = d, distance = 2))
+c.vertex.append(vertex(nodeTarget = d, distance = 2))
+
+print(a.vertex)
