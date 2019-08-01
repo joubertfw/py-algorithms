e8e62e83500cd2b2be8f3f96d3ca7b9f7aedc8a6
Joubert Filho <37818492+joubertfw@users.noreply.github.com>
2019-08-01 05:51:26 -0300

    Create dijkstra.py

---
+++ dijkstra.py
@@ -0,0 +1,16 @@
+import math as m
+
+class node():
+    def __init__(self, nodeId = 0, nodes = [], vextexs = [], visited = False):
+        self.nodeId = nodeId
+        self.nextNode = nextNode
+        self.vextexCost = nextNvextexCostode
+        self.visited = visited
+
+# entrada: "a, b, c" onde a = idOrigem, b = idDestino, c= custo de a para b
+
+graph = node(nodeId = 1, vextexCost = 0, visited = False)
+
+for i in input:
+    x = node
+
