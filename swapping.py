7497eff242750d0fec42e871fb0aa5b365c66a5d
Joubert Filho <37818492+joubertfw@users.noreply.github.com>
2019-08-21 11:24:53 -0300

    Update swapping.py

--- swapping.py
+++ swapping.py
@@ -1,17 +1,20 @@
 class Process:
-    def __init__(self, name = "Unnamed", size = 1, id = 0):
+    def __init__(self, name = "Unnamed", size = 1, id = 0, position = 0):
         self.name = name
         self.size = size
+        self.position = position
+        self.position = position
         self.id = id
 
     def __repr__(self):
-        return "({}) {} -> {}M".format(self.id, self.name, self.size)
+        return "({}) {} -> {}M  - pos {}".format(self.id, self.name, self.size, self.position)
 
 # Dificuldades: pensar na lógica de memória, usar classe? dicionário? uma classe para memória?
 # Usar id implica usar classe para toda a memória
 
 class Memory:
     memory = [None] * 100
+    processList = []
     lastId = 1
 
 # 1
@@ -34,15 +37,21 @@ class Memory:
 
 # Em python nao da pra mudar o iterador dentro do for, logo essa opção não e util 
 # 2
+    # def show(self, prog = None):
+    #     if prog is not None:
+    #         print(prog)
+    #     for i in range(0, len(self.memory)):
+    #         if self.memory[i] is not None:
+    #             print(self.memory[i])
+    #             print(i)
+    #             i += int(self.memory[i].size)
+    #             print(i)
+
     def show(self, prog = None):
         if prog is not None:
             print(prog)
-        for i in range(0, len(self.memory)):
-            if self.memory[i] is not None:
-                print(self.memory[i])
-                print(i)
-                i += int(self.memory[i].size)
-                print(i)
+        for i in self.processList:
+            print(i)
 
 # 3
 # deletar por id ou nome, primeira opcao
@@ -66,12 +75,15 @@ class Memory:
 
     def delete(self):
         deleted = False
-        data = input("Id or name: ")
-        for process in self.memory:
-            if process.id is data or process.name is data:
-                process = None
-                deleted = True
-        return True, deleted
+        data = input("Nome ou Id: ")
+        for i in range(0, len(self.memory)):
+            process = self.memory[i]
+            if process is not None:
+                if process.id is ord(data)-48 or process.name is data:
+                    self.processList.remove(process)
+                    self.memory[i:i + int(process.size)] = [None] * int(process.size)
+                    deleted = True
+                    return True, deleted
 
 # 4
     def freeSpace(self, mem = memory):
@@ -81,18 +93,35 @@ class Memory:
     def showState(self):
         for i in range(0, len(self.memory)):
             if self.memory[i] is not None:
-                print(self.memory[i])
+                print("0x{} {}".format(i, self.memory[i]))
+            else:
+                print("0x{} None - 1M".format(i))
 
+# 6
+    def compress(self):
+        lastNone = -1
+        for i in range(0, len(self.memory)):
+            process = self.memory[i]
+            if process is None:
+                for j in range(i, len(self.memory)):
+                    process2 = process = self.memory[j]
+                    if process2 is not None:
+                        self.memory[i: i + int(process2.size)], self.memory[j: j + int(process2.size)] = self.memory[j: j + int(process2.size)], self.memory[i: i + int(process2.size)]
+                        print("next")
+                        break
 
     def firstMatch(self, process = None):
         size = int(process.size)
         for i in range(0, len(self.memory)):
-            if self.memory[i] is None and self.memory[i : size].count(None) is size:
-                self.memory[i : size] = [process] * size
+            if self.memory[i] is None and self.memory[i : i + size].count(None) is size:
+                self.memory[i : i + size] = [process] * size
+                process.position = i
+                self.processList.append(process)
+                break
 
 m = Memory()
 opt = 0
-menu = "\n1- Load Process into memory\n2- Show process in memory\n3- Remove a program from memory\n4- Show disponible space\n5- Show current state\n7- Exit\n\n"
+menu = "\n1- Load Process into memory\n2- Show process in memory\n3- Remove a program from memory\n4- Show disponible space\n5- Show current state\n6- Compress memory\n7- Exit\n"
 
 while(opt is not '7'):
     print(menu)
@@ -108,5 +137,8 @@ while(opt is not '7'):
         m.delete()
     elif opt is '4':
         m.freeSpace()
-
+    elif opt is '5':
+        m.showState()
+    elif opt is '6':
+        m.compress()
 
